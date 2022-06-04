63b87dfb3eb9f187b1142844370cbdfdde7946b4
unknown <aaronlin986@yahoo.com>
2022-06-04 13:31:19 -0400

    Included Mongo validation errors

--- index.js
+++ index.js
@@ -80,19 +80,19 @@ app.delete('/api/persons/:id', (request, response, next) => {
         .catch(error => next(error));
 });
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body;
-
-    if(!body.name || !body.number){
-        return response.status(400).json({error: "Name or number must not be empty."});
-    }
 
     const person = new Person({
         name: body.name,
         number: body.number
     });
 
-    person.save().then(savedPerson => response.json(savedPerson));
+    person.save()
+        .then(savedPerson => {
+            response.json(savedPerson)
+        })
+        .catch(error => next(error));
 });
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -113,6 +113,9 @@ const errorHandler = (error, request, response, next) => {
 
     if(error.name === "CastError"){
         return response.status(400).send({error: "Malformed ID"});
+    }
+    else if(error.name === "ValidationError"){
+        return response.status(400).json({error: error.message});
     }
 
     next(error);
